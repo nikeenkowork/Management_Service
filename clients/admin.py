e99f831ee7d3ebcb8750a74e02434734bda0355d
Denys <nikeenko89@gmail.com>
2026-07-22 23:46:58 +0200

    clients/models.py и clients/admin.py

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Recipient
 
-# Register your models here.
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ("email", "full_name", "owner")
+
